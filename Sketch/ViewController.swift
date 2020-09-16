47c9a162536eddb6b2e4b23cc3bd6b728e5dc390
Hyunsoo Kim <gustnqpf3@gmail.com>
2020-09-16 16:19:50 +0900

    Initial Commit

---
+++ Sketch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sketch
+//
+//  Created by 김현수 on 2020/09/16.
+//  Copyright © 2020 Hyun Soo Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
